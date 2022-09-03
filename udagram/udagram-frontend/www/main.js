1c4968a5c84774191d0768ce12877e09405efab7
Ahmed <ahmedkhota2010@gmail.com>
2022-09-03 20:58:13 +0200

    updates

--- udagram/udagram-frontend/www/main.js
+++ udagram/udagram-frontend/www/main.js
@@ -1955,7 +1955,8 @@ __webpack_require__.r(__webpack_exports__);
 var environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: 'http://localhost:8080/api/v0'
+    apiHost: 'http://udagram-api-dev2222222222.us-east-1.elasticbeanstalk.com/api/v0'
+    // apiHost: 'http://localhost:8080/api/v0'
 };
 /*
  * For easier debugging in development mode, you can import the following file
